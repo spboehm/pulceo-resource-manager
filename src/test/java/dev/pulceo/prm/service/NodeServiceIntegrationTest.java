b88128a49ac72eb47f1b5ad0ddb3f5a9cb44523b
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-01-22 15:55:23 +0100

    add integration test for onpremnode

--- src/test/java/dev/pulceo/prm/service/NodeServiceIntegrationTest.java
+++ src/test/java/dev/pulceo/prm/service/NodeServiceIntegrationTest.java
@@ -2,7 +2,13 @@ package dev.pulceo.prm.service;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import dev.pulceo.prm.exception.NodeServiceException;
+import dev.pulceo.prm.model.node.Node;
+import dev.pulceo.prm.model.node.NodeMetaData;
 import dev.pulceo.prm.model.node.OnPremNode;
+import dev.pulceo.prm.model.provider.OnPremProvider;
+import dev.pulceo.prm.model.provider.ProviderMetaData;
+import dev.pulceo.prm.model.provider.ProviderType;
+import dev.pulceo.prm.model.registration.CloudRegistration;
 import dev.pulceo.prm.repository.AbstractNodeRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +16,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.WireMockSpring;
 
+import java.util.UUID;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class NodeServiceIntegrationTest {
@@ -50,6 +59,10 @@ public class NodeServiceIntegrationTest {
         this.abstractNodeRepository.deleteAll();
     }
 
+    private UUID pnaUUID = UUID.fromString("0247fea1-3ca3-401b-8fa2-b6f83a469680");
+    private UUID prmUUID = UUID.fromString("ecda0beb-dba9-4836-a0f8-da6d0fd0cd0a");
+    private String prmEndpoint = "http://localhost:7878";
+
     @Test
     public void testCreateOnPremNode() throws NodeServiceException {
         // given
@@ -65,6 +78,30 @@ public class NodeServiceIntegrationTest {
         OnPremNode onPremNode = this.nodeService.createOnPremNode(providerName, hostName, "pnaInitToken");
 
         // then
+        OnPremProvider onPremProvider = OnPremProvider.builder().providerMetaData(
+                ProviderMetaData.builder()
+                        .providerName("default")
+                        .providerType(ProviderType.ON_PREM)
+                        .build()).build();
+
+        NodeMetaData nodeMetaData = NodeMetaData.builder()
+                .pnaUUID(pnaUUID)
+                .hostname(hostName)
+                .build();
+
+        Node node = Node.builder()
+                .name(hostName)
+                .build();
+
+        CloudRegistration cloudRegistration = CloudRegistration.builder().pnaUUID(pnaUUID).prmUUID(prmUUID).prmEndpoint(prmEndpoint).pnaToken("dGppWG5XamMyV2ZXYTBadzlWZ0dvWnVsOjVINHhtWUpNNG1wTXB2YzJaQjlTS2ZnNHRZcWl2OTRl").build();
+
+        OnPremNode extpectedOnPremNode = OnPremNode.builder()
+                .onPremProvider(onPremProvider)
+                .nodeMetaData(nodeMetaData)
+                .node(node)
+                .cloudRegistration(cloudRegistration)
+                .build();
+        assertEquals(extpectedOnPremNode, onPremNode);
     }
 
 
