bf3d2bd3f393860e7ed1bf29cc904156b90d0768
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2024-02-03 09:38:28 +0100

    add k6 script

--- e2e-tests/script.js
+++ e2e-tests/script.js
@@ -1,8 +1,15 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+const scheme = "http";
+const pulceoHostname = "127.0.0.1";
+const prmPort = "7878";
+const pmsPort = "7777";
+const pnaHostname1 = "127.0.0.1"
+const pnaHostname2 = "127.0.0.2"
+
 export default function () {
-  const url = 'http://127.0.0.1:7878';
+  const url = scheme + "://" + pulceoHostname + ":" + prmPort + "/api/v1/health";
   const payload = JSON.stringify({
     email: 'aaa',
     password: 'bbb',
@@ -13,6 +20,33 @@ export default function () {
       'Content-Type': 'application/json',
     },
   };
+  http.get(url, payload, params);
+}
 
-  http.post(url, payload, params);
+/* on-prem-track */
+
+// create node
+export function createNode() {
+    const url = scheme + "://" + pulceoHostname + ":" + prmPort + "/api/v1/nodes";
+    const payload = JSON.stringify({
+        "providerName": "default ",
+        "hostname": scheme + "://" + pnaHostname1 + ":" + pmsPort,
+        "pnaInitToken": "b0hRUGwxT0hNYnhGbGoyQ2tlQnBGblAxOmdHUHM3MGtRRWNsZVFMSmdZclFhVUExb0VpNktGZ296"
+    });
+    http.post(url, payload, params);
+}
+
+// retrieve by ID
+
+
+
+
+export function contacts() {
+
+  http.get('https://test.k6.io/contacts.php', {
+
+    tags: { my_custom_tag: 'contacts' },
+
+  });
+
 }
